3499adbb1250087bca0d7979f96b55591ce406d9
fran-gomez <fgvolonterio@gmail.com>
2021-07-18 14:43:23 -0300

    Migracion a la API de meteored. Reporte diario listo.

--- src/weather.py
+++ src/weather.py
@@ -1,36 +1,45 @@
 # -*- encoding: utf-8 -*-
 
-import json
 import requests
+import speak
+
+# Referencia del JSON con los resultados en
+# https://www.meteored.com.ar/documentacion_api/es/Metadata_latam.pdf
 
 class Weather:
     
+    # @brief Emite el reporte extendido del clima a 5 dias
+    # @requires Conexion a internet
+    # @returns Temperatura, presion, humedad y viento para los proximos 5 dias
     def get_forecast():
-        API_KEY = "e8db9e21c8c3494bda08cdc02e4f99f3"
-        API_URL = f'http://api.openweathermap.org/data/2.5/forecast?&id=3865086&units=metric&appid={API_KEY}'
+        API_URL = 'http://api.meteored.com.ar/index.php?api_lang=ar&localidad=16884&affiliate_id=4s8w6nd9wumv&v=3.0'
 
         response = requests.get(API_URL)
 
-        print(response.status_code)
-        text = json.dumps(response.json(), sort_keys=True, indent=2)
-        print(text)
+        return response
     
+    # @brief Emite el reporte diario del clima
+    # @requires Conexion a internet
+    # @returns Temperatura, presion, humedad y viento para el dia de hoy
     def get_weather_report():
-        API_KEY = "e8db9e21c8c3494bda08cdc02e4f99f3"
-        API_URL = f'http://api.openweathermap.org/data/2.5/weather?&id=3865086&units=metric&appid={API_KEY}'
+        API_URL = 'http://api.meteored.com.ar/index.php?api_lang=ar&localidad=16884&affiliate_id=4s8w6nd9wumv&v=3.0'
 
         response = requests.get(API_URL)
 
-        name = response.json()['name'] # Nombre de la ciudad
-        report = f'The weather on {name} is'
-        main = response.json()['main'] # Sensacion termica, humedad, presion, temperatura
-        weather = response.json()['weather'] # Descripcion
-        wind = response.json()['wind'] # Orientacion, rafaga maxima, velocidad
+        city_name = response.json()['location'][0:12]
+        today = response.json()['day']['1']['hour'][0]
+        units = response.json()['day']['1']['units']
 
-        print(f'{report}\n')
-        print(f'{main}\n')
-        print(f'{weather}\n')
-        print(f'{wind}\n')
+        temperature = f"It's around {today['temp']} degrees"
+        pressure = f"The atmospheric pressure is {today['pressure']} hectopascals"
+        humidity = f"And the humidity is {today['humidity']} percent."
+        winds = f"The winds from {today['wind']['dir']} vary, with gusts of {today['wind']['gusts']} kilometers per hour"
+
+        report = f"The weather for {city_name} today, {temperature}, {pressure}, {humidity} and {winds}"
+        print(report)
+        speak.speak(report)
+
+        return report
 
 
 Weather.get_weather_report()
