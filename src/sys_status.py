b9918ff2394b9e7d1928c8a3ad955c3d1d41c95f
fran-gomez <fgvolonterio@gmail.com>
2021-07-24 17:13:26 -0300

    Estado del hw del sistema

--- src/sys_status.py
+++ src/sys_status.py
@@ -1,3 +1,5 @@
+import math
+import platform
 import psutil
 
 class SystemStatus:
@@ -5,17 +7,33 @@ class SystemStatus:
         pass
 
     def get_system_stats(self):
+        running_since = psutil.boot_time()
+        
+    def get_cpu_stats(self):
         cpu_usage = psutil.cpu_percent(percpu=True)
+
+        max = math.max(cpu_usage)
+        min = math.min(cpu_usage)
+        avg = psutil.cpu_percent(percpu=False)
+        report = f"Maximum cpu usage is at {max} percent, minimum at {min}, and avergave at {avg}"
+
+        return report
+    
+    def get_memory_stats(self):
         mem_usage = psutil.virtual_memory()
         swap_usage = psutil.swap_memory()
-        disk_usage = psutil.disk_usage()
-        running_since = psutil.boot_time()
+        print(mem_usage)
+    
+    def get_disk_stats(self, partition):
+        disk_usage = psutil.disk_usage(partition)
+    
+    def get_battery_stats(self):
         battery_level = psutil.sensors_battery()
-        # sensors = psutil.sensors_temperatures(fahrenheit=False)
-
-        print(cpu_usage)
         print(battery_level.percent)
-        # print(sensors)
-
-sys = SystemStatus()
-sys.get_system_stats()
+    
+    def get_sensors_stats(self):
+        if (platform.system() != 'Darwin'):
+            sensors = psutil.sensors_temperatures(fahrenheit=False)
+            print(sensors)
+        else:
+            print('Not supported on M1 macs')
